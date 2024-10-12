da5f5144f18d3898d2aedcb317d234506a4f1fef
ADRIEN D <ad.villermois@gmail.com>
2024-10-12 17:35:01 +0200

    feat(prebuild) : google OAuth

--- app/Auth/Login.jsx
+++ app/Auth/Login.jsx
@@ -1,14 +1,17 @@
 import { Typo, Container, Button } from '../../components/atoms';
 import colors from '../../utils/Colors';
+import {
+  GoogleOneTapSignIn,
+  statusCodes,
+  isErrorWithCode,
+  GoogleSignin,
+} from '@react-native-google-signin/google-signin';
 
 export default function Login({ navigation }) {
   return (
     <Container.ScreenBase centered>
       <Typo.Title>Memorize It</Typo.Title>
-      <Button.ClassicButton
-        onPress={() => navigation.navigate('MainStack')}
-        backgroundColor={colors.lightPurple}
-      >
+      <Button.ClassicButton onPress={() => login()} backgroundColor={colors.lightPurple}>
         <Typo.SubTitle color={colors.white}>Se connecter</Typo.SubTitle>
       </Button.ClassicButton>
     </Container.ScreenBase>
